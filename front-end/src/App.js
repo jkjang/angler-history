a1cff03f1aaa823760acc624452a5ce35b51c2ec
김명근 <myounggeun-kim@gimmyeong-geun-ui-MacBook-Pro.local>
2018-06-01 10:21:52 +0900

    테스트

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,3 +19,6 @@ export default App;
 // />       <h1 className="App-title">Welcome to React</h1> </header>     <p
 // className="App-intro">       To get started, edit <code>src/App.js</code> and
 // save to reload.     </p>   </div> ); export default App;
+
+
+// 테스트
