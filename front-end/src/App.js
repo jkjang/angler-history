b9f6f8a658617dc77542ff32e03dc220cd4c9aa3
jkjang <jkjang81@gmail.com>
2018-06-11 10:32:25 +0900

    init

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -18,8 +18,4 @@ export default App;
 // className="App-header">       <img src={logo} className="App-logo" alt="logo"
 // />       <h1 className="App-title">Welcome to React</h1> </header>     <p
 // className="App-intro">       To get started, edit <code>src/App.js</code> and
-// save to reload.     </p>   </div> ); export default App;
-
-
-// 테스트
-// TEST2
+// save to reload.     </p>   </div> ); export default App; 테스트 TEST2
--- front-end/src/components/Feed.js
+++ front-end/src/components/Feed.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Feed extends Component {
   render() {
     return (
-      <div>피드 페이지 입니다.</div>
+      <div>
+        피드 페이지 입니다.
+      </div>
     );
   }
 }
