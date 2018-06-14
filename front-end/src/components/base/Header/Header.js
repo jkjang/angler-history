801b1ef0739de6fa7277b2e5acb5e58777e4870e
jkjang <jkjang81@gmail.com>
2018-06-15 08:10:34 +0900

    header

--- front-end/src/components/base/Header/Header.js
+++ front-end/src/components/base/Header/Header.js
@@ -4,19 +4,19 @@ import 'components/base/Header/Header.scss';
 class Header extends Component {
   render() {
     return (
-      <header className="blog-header py-3">
-        <div className="row flex-nowrap justify-content-between align-items-center">
-          <div className="col-4 pt-1">
-            <a className="text-muted" href="#">
-              Subscribe
+      <header className="header py-3">
+        <div className="row">
+          <div className="flex-3 d-flex">
+            {/* <a className="text-muted" href="#">
+
+            </a> */}
+          </div>
+          <div className="flex-3 text-center">
+            <a className="header-logo text-dark" href="#">
+              Fishing Story
             </a>
           </div>
-          <div className="col-4 text-center">
-            <a className="blog-header-logo text-dark" href="#">
-              Large
-            </a>
-          </div>
-          <div className="col-4 d-flex justify-content-end align-items-center">
+          <div className="flex-3 d-flex justify-content-end align-items-center">
             <a className="text-muted" href="#">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
